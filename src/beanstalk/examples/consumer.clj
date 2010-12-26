28f1e656d8cbcbd630db20e3b44a5611717c0c49
Damon Snyder <drsnyder@gmail.com,damon@huddler-inc.com>
2010-12-25 19:09:49 -0800

    Adding some examples.

---
+++ src/beanstalk/examples/consumer.clj
@@ -0,0 +1,31 @@
+(ns beanstalk.examples.consumer
+  (:gen-class)
+  (:use clojure.contrib.command-line)
+  (:use beanstalk.core))
+
+; .clojure
+; ./src:./classes:./lib:beanstalk-1.0.0-SNAPSHOT.jar
+; (compile 'beanstalk.examples.consumer)
+; java -cp ./classes/:./lib/clojure-1.2.0.jar:./lib/clojure-contrib-1.2.0.jar beanstalk.examples.consumer
+
+(defn -main [& args]
+  (with-command-line args
+                     "Beanstalk simple consumer process"
+                     [[host "The host to connect to" "localhost"]
+                      [port "This is the description for bar" 11300]
+                      [tube "The tube to \"use\"." "beanstalk-clj-test"]
+                      remaining]
+
+                     (println (str "Using host " host " port " port " and tube " tube))
+
+                     (with-open [b (new-beanstalk host port)]
+                       (let [ret (.watch b tube)]
+                         (if ret
+                           (loop [job (.reserve b)]
+                             (if (not (= (:payload job) "exit"))
+                               (do
+                                 (println "=> " (:payload job))
+                                 (.delete b (:id job))
+                                 (recur (.reserve b)))
+                               (println "Exiting.")))
+                           (println (str "Error, watch failed: " ret)))))))
---
+++ src/beanstalk/examples/producer.clj
@@ -0,0 +1,31 @@
+(ns beanstalk.examples.producer
+  (:gen-class)
+  (:use clojure.contrib.command-line)
+  (:use beanstalk.core))
+
+; .clojure
+; ./src:./classes:./lib:beanstalk-1.0.0-SNAPSHOT.jar
+; (compile 'beanstalk.examples.producer)
+; java -cp ./classes/:./lib/clojure-1.2.0.jar:./lib/clojure-contrib-1.2.0.jar beanstalk.examples.producer
+
+(defn -main [& args]
+  (with-command-line args
+                     "Beanstalk simple producer"
+                     [[host "The host to connect to" "localhost"]
+                      [port "This is the description for bar" 11300]
+                      [tube "The tube to \"use\"." "beanstalk-clj-test"]
+                      [message m "The message to send" "hello"]
+                      [iterations n "The number of iterations" 5]
+                      remaining]
+
+                     (println (str "Using host " host " port " port " and tube " tube))
+
+                     (with-open [b (new-beanstalk host port)]
+                       (let [ret (.use b tube)]
+                         (if ret
+                           (loop [count (Integer. iterations)]
+                             (when (> count 0)
+                               (.put b 0 0 0 (.length message) message)
+                             (recur (dec count))))
+                           (println (str "Error, watch failed: " ret)))))))
+
